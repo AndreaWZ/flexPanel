ef4ccd1c8353db04b79423c84470fff82d9f1c58
Andrea Wu <lixianhyun@gmail.com>
2020-11-01 13:33:40 -0800

    add/edit images styles using JS

--- main.js
+++ main.js
@@ -6,13 +6,19 @@ function selectImages() {
     let container = document.getElementsByClassName("panel");
     let first = document.querySelectorAll(".first");
     let last = document.querySelectorAll(".last");
-    // console.log(container);
-    // console.log(first);
     for(let i = 0; i < container.length; i++){
-        // console.log(container[i]);
         container[i].addEventListener("click", function(){
-            first[i].classList.add("effectF");
-            last[i].classList.add("effectL");
+            first[i].classList.toggle("firstEffect");
+            first[i].classList.toggle("first");
+            last[i].classList.toggle("lastEffect");
+            last[i].classList.toggle("last");
+            container[i].classList.toggle("imgEffect");
+            console.log(container[i].classList.length);
+            if(container[i].classList.length == 2){
+                this.style.padding = "0 60px 0 60px";
+            } else {
+                container[i].style.padding = "0 25px 0 25px";
+            }
         })
     }
 }
