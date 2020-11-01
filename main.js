fa4ce3c116ae8a48e6dc47305059b348c3eeefe1
Andrea Wu <lixianhyun@gmail.com>
2020-10-31 19:33:05 -0700

    add animation to first/last childs of 'p'

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 window.onload = function() {
-
+    selectImages();
 };
+
+function selectImages() {
+    let container = document.getElementsByClassName("panel");
+    let first = document.querySelectorAll(".first");
+    let last = document.querySelectorAll(".last");
+    // console.log(container);
+    // console.log(first);
+    for(let i = 0; i < container.length; i++){
+        // console.log(container[i]);
+        container[i].addEventListener("click", function(){
+            first[i].classList.add("effectF");
+            last[i].classList.add("effectL");
+        })
+    }
+}
